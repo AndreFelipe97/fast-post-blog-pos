285895f731da148ba717103c7f467fed4d38bb9f
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2023-10-23 16:38:17 -0300

    feat: add style 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,40 +1,18 @@
 import * as React from "react"
 import { Link, HeadFC, PageProps } from "gatsby"
 import { Layout } from "../components/_layout"
-import { Container } from "../styles/pages/404/styles"
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import { Container, Paragraph, Title } from "../styles/pages/404/styles"
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
     <Layout>
       <Container>
-        <h1 style={headingStyles}>Página não encontrada</h1>
-        <p style={paragraphStyles}>
+        <Title>Página não encontrada</Title>
+        <Paragraph>
           Desculpa 😔, Não encontramos o que você procura.
           <br />
           <Link to="/">Voltar para página principal</Link>.
-        </p>
+        </Paragraph>
       </Container>
     </Layout>
   )
--- src/styles/pages/404/styles.ts
+++ src/styles/pages/404/styles.ts
@@ -6,3 +6,13 @@ export const Container = styled.div`
   justify-content: center;
   align-items: center;
 `;
+
+export const Title = styled.h1`
+  margin-top: 0;
+  margin-bottom: 64px;
+  max-width: 320px;
+`;
+
+export const Paragraph = styled.p`
+  margin-bottom: 48px,
+`
