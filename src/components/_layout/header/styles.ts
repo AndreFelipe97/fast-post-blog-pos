32c7c649ef352de78a1e1f543e53d5f180c9cad1
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2023-10-23 12:06:24 -0300

    refactor: add responsive design on header component

--- src/components/_layout/header/styles.ts
+++ src/components/_layout/header/styles.ts
@@ -8,10 +8,23 @@ export const Container = styled.header`
   width: 100%;
   height: 10rem;
   background-color: #202024;
+
+  @media (max-width: 799px) {
+    height: 7rem;
+
+    img {
+      width: 4.5rem;
+      height: 4.1rem;
+    }
+  }
 `;
 
 export const Title = styled.h1`
   color: #e1e1e6;
   font-weight: 700;
   font-size: 2.5rem;
+
+  @media (max-width: 799px) {
+    height: 1.8rem;
+  }
 `;
